8095c2dc088744cc45ae7d25e1af9e11ab62f2c8
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2020-02-14 21:47:18 -0800

    edit examples for sorting 1st then 2nd elements

--- Data-Structures/modify_key_4_sorting.py
+++ Data-Structures/modify_key_4_sorting.py
@@ -17,7 +17,7 @@ print(sorted_numbers)
 # [(1, 5), (3, 2), (3, 3), (3, 3), (4, 9), (4, 10)]
 
 # test strings
-names = ['Vicky', 'Nguyen', 'Naruto', 'hokage', 'sasuke']
+names = ['vicky', 'nguyen', 'naruto', 'hokage', 'sasuke']
 sorted_name = sorted(names, key=functools.cmp_to_key(cmp_key))
 print(sorted_name)
-# ['Naruto', 'Nguyen', 'Vicky', 'hokage', 'sasuke']
+# ['hokage', 'naruto', 'nguyen', 'sasuke', 'vicky']

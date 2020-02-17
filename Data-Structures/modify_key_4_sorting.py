f6825365fa0a8735b59ca033887c4f23a4e3c925
VickyNguyen99 <44889544+VickyNguyen99@users.noreply.github.com>
2020-02-16 21:22:37 -0800

    two methods to custom sort list of tuples in python

--- Data-Structures/modify_key_4_sorting.py
+++ Data-Structures/modify_key_4_sorting.py
@@ -1,23 +1,20 @@
-# sort a list of tuples or strings by comparing their 1st element then 2nd element
+# custom sort a list of tuples or strings
+numbers = [(4, 10), (4, 9), (3, 3), (3, 2), (3, 3), (1, 5)] # [(1, 5), (3, 2), (3, 3), (3, 3), (4, 9), (4, 10)]
+names = ['vicky', 'nguyen', 'vampire', 'naruto', 'sasuke'] # ['hokage', 'naruto', 'nguyen', 'sasuke', 'vicky']
+
+# METHOD 1:
+numbers_1 = sorted(numbers, key=lambda x: (x[0], x[1]))
+names_1 = sorted(names, key=lambda x: (x[0], x[1]))
+
+# METHOD 2:
 import functools
 
-def cmp_key(x, y):
+def custom_cmp_key(x, y):
     if x[0] == y[0]:
         if x[1] == y[1]:
             return 0
-        else:
-            return 1 if x[1] > y[1] else -1
-    else:
-        return 1 if x[0] > y[0] else -1
+        return 1 if x[1] > y[1] else -1
+    return 1 if x[0] > y[0] else -1
 
-# test tuples
-numbers = [(4, 10), (4, 9), (3, 3), (3, 2), (3, 3), (1, 5)]
-sorted_numbers = sorted(numbers, key=functools.cmp_to_key(cmp_key))
-print(sorted_numbers)
-# [(1, 5), (3, 2), (3, 3), (3, 3), (4, 9), (4, 10)]
-
-# test strings
-names = ['vicky', 'nguyen', 'naruto', 'hokage', 'sasuke']
-sorted_name = sorted(names, key=functools.cmp_to_key(cmp_key))
-print(sorted_name)
-# ['hokage', 'naruto', 'nguyen', 'sasuke', 'vicky']
+numbers_2 = sorted(numbers, key=functools.cmp_to_key(custom_cmp_key))
+names_2 = sorted(names, key=functools.cmp_to_key(custom_cmp_key))

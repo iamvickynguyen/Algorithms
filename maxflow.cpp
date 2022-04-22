f386ca246afb2f686d31cafdabfbf9bd6d1c8ed8
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2022-04-21 21:36:01 -0700

    keep maxflow

--- maxflow.cpp
+++ maxflow.cpp
@@ -8,91 +8,79 @@ typedef pair<ll, ll> pll;
 #define pb push_back
 
 // Reference: https://github.com/stevenhalim/cpbook-code/blob/master/ch8/maxflow.cpp
-// Note: a bit slower than stevenhalim's implementation, but more space efficient and
-// finding flow value of an edge is easier. Well |V| and |E| in maxflow problems are
-// usually small, so space is not too concerning. Using 1 adjacency list (graph) is more
-// intuitive to me than using an adjacency list and an edge list that's all.
-
+typedef tuple<int, ll, ll> edge;
 const ll INF = 1e18;
 
 class max_flow {
-	private:
-	int V;
-	vector<unordered_map<int, pll>> graph;	// adjacency list with values are pair(cap, flow)
-	vi dist, last;
+    private:
+    int V;
+    vector<edge> EL;
+    vector<vi> AL;
+    vi d, last;
+    vector<ii> p;
+    unordered_map<ll, int> AL_EL_map;
 
-	bool BFS(int s, int t) {					// find augmenting path
-		dist.assign(V, -1); dist[s] = 0;
-		queue<int> q({s});
-		while (!q.empty()) {
-			int u = q.front(); q.pop();
-			if (u == t) break;
-			for (auto it = graph[u].begin(); it != graph[u].end(); it++) {
-				int v = it->first;
-				auto &[cap, flow] = it->second;
-				if ((cap - flow > 0) && (dist[v] == -1)) {
-					dist[v] = dist[u] + 1;
-					q.push(v);
-				}
-			}
-		}
-		return dist[t] != -1;
-	}
-	
-	ll DFS(int u, int t, ll f = INF) {
-		if ((u == t) || (f == 0)) return f;
-		for (auto &edge: graph[u])	{				// from last edge
-			int v = edge.first;
-			auto &[cap, flow] = edge.second;
-			if (dist[v] != dist[u] + 1) continue;			// not part of the layer graph
-			if (ll pushed = DFS(v, t, min(f, cap - flow))) {
-				flow += pushed;
-				auto &rflow = graph[v][u].second;
-				rflow -= pushed;
-				return pushed;
-			}
-		}
-		return 0;
-	}
-	
-	public:
-	max_flow(int initialV) : V(initialV) {
-		graph.assign(V, unordered_map<int, pair<ll, ll>>());
-	}
+    bool BFS(int s, int t) {                       // find augmenting path
+        d.assign(V, -1); d[s] = 0;
+        queue<int> q({s});
+        p.assign(V, {-1, -1});                       // record BFS sp tree
+        while (!q.empty()) {
+        int u = q.front(); q.pop();
+        if (u == t) break;                       // stop as sink t reached
+        for (auto &idx : AL[u]) {                    // explore neighbors of u
+            auto &[v, cap, flow] = EL[idx];          // stored in EL[idx]
+            if ((cap-flow > 0) && (d[v] == -1))      // positive residual edge
+              d[v] = d[u]+1, q.push(v), p[v] = {u, idx}; // 3 lines in one!
+            }
+        }
+        return d[t] != -1;                           // has an augmenting path
+    }
 
-	void add_edge(int u, int v, ll w, bool directed = true) {
-		if (u == v) return;
-		graph[u][v] = {w, 0};
-		graph[v][u] = {directed ? 0 : w, 0};
-	}
+    ll DFS(int u, int t, ll f = INF) {           // traverse from s->t
+        if ((u == t) || (f == 0)) return f;
+        for (int &i = last[u]; i < (int)AL[u].size(); ++i) { // from last edge
+            auto &[v, cap, flow] = EL[AL[u][i]];
+            if (d[v] != d[u]+1) continue;                // not part of layer graph
+            if (ll pushed = DFS(v, t, min(f, cap-flow))) {
+                flow += pushed;
+                auto &rflow = get<2>(EL[AL[u][i]^1]);     // back edge
+                rflow -= pushed;
+                return pushed;
+            }
+        }
+        return 0;
+    }
 
-	ll dinitz(int s, int t) {
-		ll mf = 0;
-		while (BFS(s, t)) {
-			last.assign(V, 0);
-			while (ll f = DFS(s, t)) mf += f;
-		}
-		return mf;
-	}
+    public:
+    max_flow(int initialV) : V(initialV) {
+        EL.clear();
+        AL.assign(V, vi());
+    }
 
-	ll get_flow_edge(int u, int v) {
-		return graph[u][v].second;
-	}
+    // if you are adding a bidirectional edge u<->v with weight w into your
+    // flow graph, set directed = false (default value is directed = true)
+    void add_edge(int u, int v, ll w, bool directed = true) {
+        if (u == v) return;                          // safeguard: no self loop
+        EL.emplace_back(v, w, 0);                    // u->v, cap w, flow 0
+        AL[u].push_back(EL.size()-1);                // remember this index
+        AL_EL_map[u*V + v] = EL.size() - 1;
+        EL.emplace_back(u, directed ? 0 : w, 0);     // back edge
+        AL[v].push_back(EL.size()-1);                // remember this index
+        AL_EL_map[v*V + u] = EL.size() - 1;
+    }
+
+    ll dinic(int s, int t) {
+        ll mf = 0;                                   // mf stands for max_flow
+        while (BFS(s, t)) {                          // an O(V^2*E) algorithm
+            last.assign(V, 0);                       // important speedup
+            while (ll f = DFS(s, t))                     // exhaust blocking flow
+                mf += f;
+        }
+        return mf;
+    }
+    
+    ll get_flow_edge(int u, int v) {
+        return get<2>(EL[AL_EL_map[u*V + v]]);
+    }
 };
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-	max_flow mf(5);
-	mf.add_edge(0,1,100);
-	mf.add_edge(0,2,100);
-	mf.add_edge(1,2,1);
-	mf.add_edge(1,3,100);
-	mf.add_edge(2,3,99);
-	ll flow = mf.dinitz(0,3);
-	cout << flow << '\n';
-
-    return 0;
-}
 
